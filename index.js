c23ea39e51b98bae98e054808e80ee75174bea22
Ifa Dernegi <ifasoftbilgi@gmail.com>
2025-08-13 13:08:19 +0300

    port settings added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
 // Basit sağlık kontrolü
